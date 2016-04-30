f65f8813e2b9e88acc64ff425c64231ecc298380
Camilo1090 <camilo.ruiz.casanova@correounivalle.edu.co>
2016-04-30 03:03:28 -0500

    menu principal

---
+++ src/Controlador/MenuPrincipal_Eventos.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Vista.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+/**
+ *
+ * @author Camilo Ruiz Casanova
+ */
+public class MenuPrincipal_Eventos 
+{
+    private final MenuPrincipal menuPrincipal;
+    
+    private File archivo;
+    private String algoritmo;
+    
+    public MenuPrincipal_Eventos(final MenuPrincipal menuPrincipal)
+    {
+        this.menuPrincipal = menuPrincipal;
+        
+        this.menuPrincipal.bArchivo.addActionListener(
+            (ActionEvent ae) -> 
+            {
+                cargarArchivo();
+            }
+        );
+        
+        this.menuPrincipal.bEmpezar.addActionListener(
+            (ActionEvent ae) -> 
+            {
+                empezar();
+            }
+        );
+    }
+    
+    public void cargarArchivo()
+    {
+        this.menuPrincipal.fileChooser.setFileFilter(new FileNameExtensionFilter(".txt", "txt"));
+        int op = this.menuPrincipal.fileChooser.showOpenDialog(menuPrincipal);
+        
+        if (op == JFileChooser.APPROVE_OPTION)
+        {
+            String ruta = this.menuPrincipal.fileChooser.getSelectedFile().getAbsolutePath();
+            this.menuPrincipal.tfArchivo.setText(ruta);
+            this.archivo = new File(ruta);
+        }
+    }
+    
+    public void empezar()
+    {
+        this.algoritmo = this.menuPrincipal.cbAlgoritmos.getSelectedItem().toString();
+        
+        if (this.archivo == null)
+        {
+            JOptionPane.showMessageDialog(menuPrincipal, "Debe seleccionar un archivo.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        else
+        {
+            // mapa
+        }
+    }
+}
--- src/ProyectoIA.java
+++ src/ProyectoIA.java
@@ -1,3 +1,7 @@
+
+import Controlador.MenuPrincipal_Eventos;
+import Vista.MenuPrincipal;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,6 +16,23 @@ public class ProyectoIA
 {
     public static void main(String[] args) 
     {
-        
+        setLookAndFeel();
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+        MenuPrincipal_Eventos menuPrincipal_Eventos = new MenuPrincipal_Eventos(menuPrincipal);
+        menuPrincipal.setLocationRelativeTo(null);
+        menuPrincipal.setVisible(true);
+    }
+    
+    public static void setLookAndFeel() {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("com.sun.java.swing.plaf.windows.WindowsLookAndFeel".equals(info.getClassName()) || "com.sun.java.swing.plaf.gtk.GTKLookAndFeel".equals(info.getClassName())) {   
+                   javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                   break;
+                } 
+            }
+        } catch(Exception e) {
+          System.out.println("Error setting native LAF: " + e);
+        }
     }
 }
